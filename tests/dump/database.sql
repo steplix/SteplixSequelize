01d2287c283294bc249316b9218aa16f028a7ee8
alejandro remonda <alejandro@steplix.com>
2022-11-03 11:35:57 -0300

    Update database script and sequelize-automate dependency

--- tests/dump/database.sql
+++ tests/dump/database.sql
@@ -2,10 +2,15 @@ DROP DATABASE IF EXISTS `steplix`;
 CREATE DATABASE `steplix` CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `steplix`;
 
-# GRANT ALL PRIVILEGES ON steplix.* To 'test'@'localhost' IDENTIFIED BY 'test';
-# GRANT ALL PRIVILEGES ON steplix.* To 'test'@'%' IDENTIFIED BY 'test';
-# GRANT SELECT ON steplix.* To 'read'@'localhost' IDENTIFIED BY 'test';
-# GRANT SELECT ON steplix.* To 'read'@'%' IDENTIFIED BY 'test';
-# GRANT SELECT, INSERT, UPDATE, DELETE ON steplix.* To 'write'@'localhost' IDENTIFIED BY 'test';
-# GRANT SELECT, INSERT, UPDATE, DELETE ON steplix.* To 'write'@'%' IDENTIFIED BY 'test';
-# FLUSH PRIVILEGES;
+
+
+CREATE USER 'test'@'%' IDENTIFIED BY 'test';
+GRANT ALL PRIVILEGES ON steplix.* TO 'test'@'%';
+
+CREATE USER 'read'@'%' IDENTIFIED BY 'test';
+GRANT SELECT ON steplix.* To 'read'@'%';
+
+CREATE USER 'write'@'%' IDENTIFIED BY 'test';
+GRANT SELECT, INSERT, UPDATE, DELETE ON steplix.* To 'write'@'%';
+
+FLUSH PRIVILEGES;
